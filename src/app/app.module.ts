06e4239aef121e972e02a3969be2137ccdb57b05
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-03 16:33:21 -0500

    Hello World!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component'
 
 // tells angular to start app component at launch
+    //  registers all of the apps components
+
 @NgModule({
      imports: [BrowserModule],
-    //  registers all of the apps components
      declarations: [AppComponent],
      bootstrap: [AppComponent]
 })
