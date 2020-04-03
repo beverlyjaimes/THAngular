da7f0abade5713134aba588e14d5a5ad8b217712
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-03 16:20:48 -0500

    added app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+    // must be called the same as index.html
+    selector: 'app-root',
+    template: `<h2>Hello World!</h2>`,
+    styles: [
+        `
+        h2 {
+            font-family: sans-serif;
+            font-size: 1.2em;
+        }
+        `
+    ]
+    
+})
+export class AppComponent {
+
+}
