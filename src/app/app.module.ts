fcd1c9da96dbc770db61415a3133ef12b830dd58
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-05 01:06:12 -0500

    updaded barrel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'
-import {EntryListComponent} from './entries';
+import {EntryListComponent, EntryComponent} from './entries';
 
 // tells angular to start app component at launch
     //  registers all of the apps components
