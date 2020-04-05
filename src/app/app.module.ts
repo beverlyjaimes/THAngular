b793f32c59cb73a66c639bbd7e9c3f7b0703d2d8
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-05 01:32:05 -0500

    updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import {EntryListComponent, EntryComponent} from './entries';
 
 // tells angular to start app component at launch
     //  registers all of the apps components
-
+// put child components first
 @NgModule({
      imports: [BrowserModule],
      declarations: [
          AppComponent, 
+         EntryComponent,
          EntryListComponent
     ],
      bootstrap: [AppComponent]
