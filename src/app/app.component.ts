13afa08781167211bff91e6cba65c92daa94cafc
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-04 22:38:29 -0500

    added html and css files removed properties from ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     // must be called the same as index.html
     selector: 'app-root',
-    template: `<h2>Hello World!</h2>`,
-    styles: [
-        `
-        h2 {
-            font-family: sans-serif;
-            font-size: 1.2em;
-        }
-        `
-    ]
-    
+  
 })
 export class AppComponent {
 
