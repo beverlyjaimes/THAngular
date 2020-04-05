7e870bb37f0f9f551587a65013a8ffe8445faf28
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-05 00:00:16 -0500

    updated git ignore and able to use random emoji click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
  // property needs to be first defined in the class
 export class AppComponent {
-    emoji: ['👽', '💀', '🧠'];
+    emoji = ['👽', '💀', '🧠'];
     activeEmoji: string; 
     changeEmoji() {
          this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)];
